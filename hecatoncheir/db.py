7bba1ad2331e4366e299ff7a9b18032d6955ed69
Satoshi Nagayasu <snaga@uptime.jp>
2017-12-07 09:53:26 +0000

    Add new Tag2 class and the db module to access to the repository.

---
+++ hecatoncheir/db.py
@@ -0,0 +1,51 @@
+import unittest
+
+import sqlalchemy as sa
+
+conn = None
+creds = None
+
+
+def connect():
+    global creds
+    global conn
+    assert isinstance(creds, dict)
+
+    host = creds.get('host', 'localhost')
+    port = creds.get('port', 5432)
+    dbname = creds.get('dbname', 'postgres')
+    user = creds.get('username', 'postgres')
+    password = creds.get('password', '')
+
+    connstr = 'postgresql://{3}:{4}@{0}:{1}/{2}'.format(host, port, dbname, user, password)
+
+    engine = sa.create_engine(connstr)
+    conn = engine.connect()
+    return conn
+
+
+def version():
+    global conn
+    rs = conn.execute("SELECT version()")
+    for r in rs:
+        return r[0]
+
+
+class TestTag2(unittest.TestCase):
+    def setUp(self):
+        global creds
+        creds = {}
+        creds['username'] = 'postgres'
+        creds['password'] = 'postgres'
+        creds['dbname'] = 'datacatalog'
+
+    def test_connect_001(self):
+        self.assertIsNotNone(connect())
+
+    def test_version_001(self):
+        connect()
+        self.assertTrue(version().startswith('PostgreSQL '))
+
+
+if __name__ == '__main__':
+    unittest.main()
