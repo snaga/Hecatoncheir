bc9c17e5bb73b51b6801216680fadcb5c8899dcd
Satoshi Nagayasu <snaga@uptime.jp>
2017-12-07 10:59:32 +0000

    Add new table module to handle table data.

---
+++ hecatoncheir/utils.py
@@ -0,0 +1,22 @@
+import json
+
+class DbProfilerJSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        try:
+            iterable = iter(o)
+        except TypeError:
+            pass
+        else:
+            return list(iterable)
+
+        if isinstance(o, datetime.datetime):
+            return o.isoformat()
+        elif isinstance(o, datetime.date):
+            return o.isoformat()
+        elif isinstance(o, decimal.Decimal):
+            return (str(o) for o in [o])
+        return super(DbProfilerJSONEncoder, self).default(o)
+
+def jsonize(data):
+    return json.dumps(data, cls=DbProfilerJSONEncoder, sort_keys=True,
+                      indent=2)
